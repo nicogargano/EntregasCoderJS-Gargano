fab80230b548cfa7e07e0c783aafe8a5dbedc44f
nicogargano <nicolas.gargano.16@gmail.com>
2022-06-02 13:45:11 -0300

    Segunda Entrega del Proyecto Final

--- js/script.js
+++ js/script.js
@@ -1,260 +1,269 @@
 //SETEO DE MEMBRESIAS
 
-let boton6 = document.getElementById("b6");
-boton6.onclick = () => {
-    window.location.href="./pages/seteo.html";
-}
+// let boton6 = document.getElementById("b6");
+// boton6.onclick = () => {
+//     window.location.href = "./pages/seteo.html";
+// }
 
-class membresia {
-    constructor(tipo, precio, detalle, cupo) {
-        this.tipo = tipo;
-        this.precio = parseInt(precio);
-        this.detalle = detalle;
-        this.cupo = parseInt(cupo);
+let btnE = document.getElementById("bEnviar");
+let btnF = document.getElementById("bFin");
+let tipo = document.getElementById("tipo");
+let precio = document.getElementById("precio");
+let cupo = document.getElementById("cupos");
+let detalle = document.getElementById("detalle");
+p = document.querySelector("p")
+const arrayMembresias = [];
+
+btnE.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let membresia = {
+        tipoM: tipo.value,
+        precioM: precio.value,
+        detalleM: detalle.value,
+        cupoM: cupo.value
     }
-}
 
-const element = document.getElementById("bEnviar");
-element.addEventListener("click", agregar);
-
-const tipoM = document.getElementById("validationCustom01")
-const precioM = document.getElementById("validationCustom02")
-const cupoM = document.getElementById("validationCustom03")
-const detalleM = document.getElementById("validationTextarea.value")
-
-function agregar() {
-    const arrayMembresias = [];
-    let tipoM = validationCustom01.value;
-    let precioM = validationCustom02.value;
-    let detalleM = validationTextarea.value;
-    let cupoM = validationCustom03.value;
-    arrayMembresias.push(new membresia(tipoM, precioM, detalleM, cupoM));
-    console.log(arrayMembresias);
-}
-
-//BUSCAR UNA MEMBRESIA POR TIPO
-
-const btnBuscar = document.querySelector("#b5");
-btnBuscar.addEventListener("click", () => {
-
-    let busqueda = document.getElementById("busqueda");
-    let listaBusqueda = document.createElement("div");
-
-    let membIngresada = arrayMembresias.filter(membresia => membresia.tipo.includes(bMembresia.value));
-    console.log(membIngresada);
-
-    busqueda.innerHTML = "<h2> Lista de Membresias que corresponen a tu busqueda: </h2>"
-
-    membIngresada.forEach(p => {
-        let h3 = document.createElement("h3");
-        let li = document.createElement("li");
-
-        h3.innerText = p.tipo;
-
-        li = document.createElement("li");
-        li.innerText = p.detalle;
-        h3.appendChild(li);
-
-        li = document.createElement("li");
-        li.innerText = p.precio;
-        h3.appendChild(li);
-
-        li = document.createElement("li");
-        li.innerText = p.cupo;
-        h3.appendChild(li);
-
-        listaBusqueda.appendChild(h3)
-    });
-
-    busqueda.appendChild(listaBusqueda);
-
+    if (membresia.tipoM == "" || membresia.precioM == "" || membresia.detalle == "" || membresia.cupoM == "") {
+        p.innerText = "Campos vacios!";
+        return;
+    } else {
+        arrayMembresias.push(membresia);
+        localStorage.setItem("item", JSON.stringify(arrayMembresias));
+        tipo.value = ""
+        precio.value = ""
+        detalle.value = ""
+        cupo.value = ""
+        p.innerText = "Guardado con exito!"
+    }
+    return mem;
 });
 
-// MEMBRESIAS
 
-let tabla = document.getElementById("tMembresias");
-let cuerpoTabla = document.createElement("tbody");
 
-arrayMembresias.forEach(p => {
-    let fila = document.createElement("tr");
-    let td = document.createElement("td");
+// //BUSCAR UNA MEMBRESIA POR TIPO
 
-    td.innerText = p.tipo;
-    fila.appendChild(td);
+// const btnBuscar = document.querySelector("#b5");
+// btnBuscar.addEventListener("click", () => {
 
-    td = document.createElement("td");
-    td.innerText = p.detalle;
-    fila.appendChild(td);
+//     let busqueda = document.getElementById("busqueda");
+//     let listaBusqueda = document.createElement("div");
 
-    td = document.createElement("td");
-    td.innerText = p.precio;
-    fila.appendChild(td);
+//     let membIngresada = arrayMembresias.filter(membresia => membresia.tipo.includes(bMembresia.value));
+//     console.log(membIngresada);
 
-    td = document.createElement("td");
-    td.innerText = p.cupo;
-    fila.appendChild(td);
+//     busqueda.innerHTML = "<h2> Lista de Membresias que corresponen a tu busqueda: </h2>"
 
-    cuerpoTabla.appendChild(fila)
-});
+//     membIngresada.forEach(p => {
+//         let h3 = document.createElement("h3");
+//         let li = document.createElement("li");
 
-tabla.appendChild(cuerpoTabla);
+//         h3.innerText = p.tipo;
 
-// SI TENGO MENOS DE 5 CUPOS EN ALGUNA MEMBRESIA
+//         li = document.createElement("li");
+//         li.innerText = p.detalle;
+//         h3.appendChild(li);
 
-let boton1 = document.getElementById("b1");
-boton1.onclick = () => {
+//         li = document.createElement("li");
+//         li.innerText = p.precio;
+//         h3.appendChild(li);
 
-    let data5 = document.getElementById("data5");
-    let listaData5 = document.createElement("div")
+//         li = document.createElement("li");
+//         li.innerText = p.cupo;
+//         h3.appendChild(li);
 
-    let pocosCupos = arrayMembresias.filter(membresia => membresia.cupo <= 5);
-    console.log(("Membresias con pocos cupos, reorganizar!"));
-    console.log(pocosCupos);
-    data5.innerHTML = "<h2> Las siguientes Membresias tienen menos de 5 cupos: </h2>";
+//         listaBusqueda.appendChild(h3)
+//     });
 
-    pocosCupos.forEach(p => {
-        let h3 = document.createElement("h3");
-        let li = document.createElement("li");
+//     busqueda.appendChild(listaBusqueda);
 
-        h3.innerText = p.tipo;
+// });
 
-        li = document.createElement("li");
-        li.innerText = p.detalle;
-        h3.appendChild(li);
+// // MEMBRESIAS
 
-        li = document.createElement("li");
-        li.innerText = p.precio;
-        h3.appendChild(li);
+// let tabla = document.getElementById("tMembresias");
+// let cuerpoTabla = document.createElement("tbody");
 
-        li = document.createElement("li");
-        li.innerText = p.cupo;
-        h3.appendChild(li);
+// arrayMembresias.forEach(p => {
+//     let fila = document.createElement("tr");
+//     let td = document.createElement("td");
 
-        listaData5.appendChild(h3)
-    });
+//     td.innerText = p.tipo;
+//     fila.appendChild(td);
 
-    data5.appendChild(listaData5);
+//     td = document.createElement("td");
+//     td.innerText = p.detalle;
+//     fila.appendChild(td);
 
-}
+//     td = document.createElement("td");
+//     td.innerText = p.precio;
+//     fila.appendChild(td);
 
-//MEMBRESIA SIN CUPO
+//     td = document.createElement("td");
+//     td.innerText = p.cupo;
+//     fila.appendChild(td);
 
-let boton2 = document.getElementById("b2");
-boton2.onclick = () => {
+//     cuerpoTabla.appendChild(fila)
+// });
 
-    let data0 = document.getElementById("data0");
-    let listaData0 = document.createElement("div");
+// tabla.appendChild(cuerpoTabla);
 
-    let noCupo = arrayMembresias.filter(membresia => membresia.cupo == 0);
-    console.log(("Hay membresias sin cupos, reorganizar ya!"));
-    console.log(noCupo);
-    data0.innerHTML = "<h2> Las siguientes Membresias no tiene cupo: </h2>";
+// // SI TENGO MENOS DE 5 CUPOS EN ALGUNA MEMBRESIA
 
-    noCupo.forEach(p => {
-        let h3 = document.createElement("h3");
-        let li = document.createElement("li");
+// let boton1 = document.getElementById("b1");
+// boton1.onclick = () => {
 
-        h3.innerText = p.tipo;
+//     let data5 = document.getElementById("data5");
+//     let listaData5 = document.createElement("div")
 
-        li = document.createElement("li");
-        li.innerText = p.detalle;
-        h3.appendChild(li);
+//     let pocosCupos = arrayMembresias.filter(membresia => membresia.cupo <= 5);
+//     console.log(("Membresias con pocos cupos, reorganizar!"));
+//     console.log(pocosCupos);
+//     data5.innerHTML = "<h2> Las siguientes Membresias tienen menos de 5 cupos: </h2>";
 
-        li = document.createElement("li");
-        li.innerText = p.precio;
-        h3.appendChild(li);
+//     pocosCupos.forEach(p => {
+//         let h3 = document.createElement("h3");
+//         let li = document.createElement("li");
 
-        li = document.createElement("li");
-        li.innerText = p.cupo;
-        h3.appendChild(li);
+//         h3.innerText = p.tipo;
 
-        listaData0.appendChild(h3)
-    });
+//         li = document.createElement("li");
+//         li.innerText = p.detalle;
+//         h3.appendChild(li);
 
-    data0.appendChild(listaData0);
+//         li = document.createElement("li");
+//         li.innerText = p.precio;
+//         h3.appendChild(li);
 
-}
+//         li = document.createElement("li");
+//         li.innerText = p.cupo;
+//         h3.appendChild(li);
 
-//ORDENAR POR CUPOS
+//         listaData5.appendChild(h3)
+//     });
 
-let boton3 = document.getElementById("b3");
-boton3.onclick = () => {
+//     data5.appendChild(listaData5);
 
-    let oCupo = document.getElementById("oCupo");
-    let listaCupo = document.createElement("div");
+// }
 
-    let ordenaCupos = [];
-    ordenaCupos = arrayMembresias.map(elemento => elemento);
-    ordenaCupos.sort(function (a, b) {
-        return a.cupo - b.cupo;
-    });
-    console.log("Ordenados por cantidad de cupos")
-    console.log(ordenaCupos);
-    oCupo.innerHTML = "<h2> Orden de Membresias segun sus cupos: </h2>";
+// //MEMBRESIA SIN CUPO
 
-    ordenaCupos.forEach(p => {
-        let h3 = document.createElement("h3");
-        let li = document.createElement("li");
+// let boton2 = document.getElementById("b2");
+// boton2.onclick = () => {
 
-        h3.innerText = p.tipo;
+//     let data0 = document.getElementById("data0");
+//     let listaData0 = document.createElement("div");
 
-        li = document.createElement("li");
-        li.innerText = p.detalle;
-        h3.appendChild(li);
+//     let noCupo = arrayMembresias.filter(membresia => membresia.cupo == 0);
+//     console.log(("Hay membresias sin cupos, reorganizar ya!"));
+//     console.log(noCupo);
+//     data0.innerHTML = "<h2> Las siguientes Membresias no tiene cupo: </h2>";
 
-        li = document.createElement("li");
-        li.innerText = p.precio;
-        h3.appendChild(li);
+//     noCupo.forEach(p => {
+//         let h3 = document.createElement("h3");
+//         let li = document.createElement("li");
 
-        li = document.createElement("li");
-        li.innerText = p.cupo;
-        h3.appendChild(li);
+//         h3.innerText = p.tipo;
 
-        listaCupo.appendChild(h3)
-    });
+//         li = document.createElement("li");
+//         li.innerText = p.detalle;
+//         h3.appendChild(li);
 
-    oCupo.appendChild(listaCupo);
+//         li = document.createElement("li");
+//         li.innerText = p.precio;
+//         h3.appendChild(li);
 
-}
+//         li = document.createElement("li");
+//         li.innerText = p.cupo;
+//         h3.appendChild(li);
 
-//ORDENAR POR VALOR
+//         listaData0.appendChild(h3)
+//     });
 
-let boton4 = document.getElementById("b4");
-boton4.onclick = () => {
+//     data0.appendChild(listaData0);
 
-    let oPrecio = document.getElementById("oPrecio");
-    let listaPrecio = document.createElement("div");
+// }
 
-    let ordenaPrecio = [];
-    ordenaPrecio = arrayMembresias.map(elemento => elemento);
-    ordenaPrecio.sort(function (a, b) {
-        return a.precio - b.precio;
-    });
-    console.log("Ordenados por valor de la Membresia")
-    console.log(ordenaPrecio);
-    oPrecio.innerHTML = "<h2> Orden de Membresias segun sus precios: </h2>";
+// //ORDENAR POR CUPOS
 
-    ordenaPrecio.forEach(p => {
-        let h3 = document.createElement("h3");
-        let li = document.createElement("li");
+// let boton3 = document.getElementById("b3");
+// boton3.onclick = () => {
 
-        h3.innerText = p.tipo;
+//     let oCupo = document.getElementById("oCupo");
+//     let listaCupo = document.createElement("div");
 
-        li = document.createElement("li");
-        li.innerText = p.detalle;
-        h3.appendChild(li);
+//     let ordenaCupos = [];
+//     ordenaCupos = arrayMembresias.map(elemento => elemento);
+//     ordenaCupos.sort(function (a, b) {
+//         return a.cupo - b.cupo;
+//     });
+//     console.log("Ordenados por cantidad de cupos")
+//     console.log(ordenaCupos);
+//     oCupo.innerHTML = "<h2> Orden de Membresias segun sus cupos: </h2>";
 
-        li = document.createElement("li");
-        li.innerText = p.precio;
-        h3.appendChild(li);
+//     ordenaCupos.forEach(p => {
+//         let h3 = document.createElement("h3");
+//         let li = document.createElement("li");
 
-        li = document.createElement("li");
-        li.innerText = p.cupo;
-        h3.appendChild(li);
+//         h3.innerText = p.tipo;
 
-        listaPrecio.appendChild(h3)
-    });
+//         li = document.createElement("li");
+//         li.innerText = p.detalle;
+//         h3.appendChild(li);
 
-    oPrecio.appendChild(listaPrecio);
+//         li = document.createElement("li");
+//         li.innerText = p.precio;
+//         h3.appendChild(li);
 
-}
+//         li = document.createElement("li");
+//         li.innerText = p.cupo;
+//         h3.appendChild(li);
+
+//         listaCupo.appendChild(h3)
+//     });
+
+//     oCupo.appendChild(listaCupo);
+
+// }
+
+// //ORDENAR POR VALOR
+
+// let boton4 = document.getElementById("b4");
+// boton4.onclick = () => {
+
+//     let oPrecio = document.getElementById("oPrecio");
+//     let listaPrecio = document.createElement("div");
+
+//     let ordenaPrecio = [];
+//     ordenaPrecio = arrayMembresias.map(elemento => elemento);
+//     ordenaPrecio.sort(function (a, b) {
+//         return a.precio - b.precio;
+//     });
+//     console.log("Ordenados por valor de la Membresia")
+//     console.log(ordenaPrecio);
+//     oPrecio.innerHTML = "<h2> Orden de Membresias segun sus precios: </h2>";
+
+//     ordenaPrecio.forEach(p => {
+//         let h3 = document.createElement("h3");
+//         let li = document.createElement("li");
+
+//         h3.innerText = p.tipo;
+
+//         li = document.createElement("li");
+//         li.innerText = p.detalle;
+//         h3.appendChild(li);
+
+//         li = document.createElement("li");
+//         li.innerText = p.precio;
+//         h3.appendChild(li);
+
+//         li = document.createElement("li");
+//         li.innerText = p.cupo;
+//         h3.appendChild(li);
+
+//         listaPrecio.appendChild(h3)
+//     });
+
+//     oPrecio.appendChild(listaPrecio);
+
+// }
